80905ee72743dd1439fd6cad268074e5adbb3ace
lucky2uhqt <72762740+lucky2uhqt@users.noreply.github.com>
2025-10-20 22:34:15 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
+from app.budget import app
 
-from my_paldea import create_app
-app = create_app()
-#app.env="development" to run the application in the developement environ
-# the reloader indicates the application is being run in debug mode
-# and that the application will reload whenever a change is made in the code
-if __name__=='__main__':
-   app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
