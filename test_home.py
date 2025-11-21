192cee61bd9861df834dcf2865a80e3f6f88f851
DoctorManish <mrshrivastav@gmail.com>
2025-11-20 17:07:42 -0800

    Delete test_home.py

--- test_home.py
+++
@@ -1,8 +0,0 @@
-def test_home_page_loads_for_logged_in_user(logged_in_client):
-    resp = logged_in_client.get("/home")
-    assert resp.status_code == 200
-    assert (
-        b"budget" in resp.data.lower()
-        or b"transaction" in resp.data.lower()
-        or b"dashboard" in resp.data.lower()
-    )
